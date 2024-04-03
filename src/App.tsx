bba7aab8fd535004c60815c0b9b7e80dfeebdf07
Zeeshan <zbr3516@gmail.com>
2024-04-04 04:09:55 +0530

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+// import { Sign } from 'crypto';
+import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Login />
+      <Signup />
+    </>
   );
 }
 
---
+++ src/Login.tsx
@@ -0,0 +1,40 @@
+import './Login.css'
+import Image from './images/com.gif'
+function Login() {
+  return (
+    <div id='bck'>
+      <div className="container">
+        <div id='bang' className="row">
+          <div id='header' className="col mt-2 mb-2">
+            <h2>Company Name</h2>
+          </div>
+        </div>
+
+        <div className="row">
+          <div id='comimg' className="col-6">
+            <img src={Image} alt="pogo" />
+          </div>
+          <div className="col-6 text-center mt-2">
+            <h2>Welcome</h2>
+            <p>Login here</p>
+            <div className="input-icons ">
+              <i className="fa fa-envelope icon"></i>
+              <input className="form-control input-field me-2 ps-4" type="search" placeholder=" Email" aria-label="Search" />
+              </div>
+              <div className="input-icons ">
+              <i className="fa fa-key icon"></i>
+              <input className="form-control input-field me-2 mt-2 ps-4" type="search" placeholder=" Password" aria-label="Search" />
+            </div>
+            <div>
+            <button className="btn mt-2 btn-block" type="submit">Login</button>
+            <button className="btn btn-outline-success mt-2 btnn1" type="submit">Sign Up</button>
+            <button className="btn btn-outline-success mt-2 btnn1" type="submit">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
